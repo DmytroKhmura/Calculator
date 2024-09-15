cabc1b426009e1a932609c964db4d84ad6e6364b
Dmytro Khmura <180873731+DmytroKhmura@users.noreply.github.com>
2024-09-16 00:05:31 +0200

    rewrite completely script.js due to changes in application's functioning logic

--- script.js
+++ script.js
@@ -1,121 +1,199 @@
-// Check //////////////////////////////////////////////////////////////////////
-class Check {
-  static checkId(id) {
-    if (typeof id !== "string") throw new TypeError("Invalid <id> type.");
-    if (id.length === 0) throw new RangeError("Invalid <id> value.");
-  }
+// Symbols ////////////////////////////////////////////////////////////////////
+const SMB_NUM_DIGIT_0 = "0";
+const SMB_NUM_DIGIT_1 = "1";
+const SMB_NUM_DIGIT_2 = "2";
+const SMB_NUM_DIGIT_3 = "3";
+const SMB_NUM_DIGIT_4 = "4";
+const SMB_NUM_DIGIT_5 = "5";
+const SMB_NUM_DIGIT_6 = "6";
+const SMB_NUM_DIGIT_7 = "7";
+const SMB_NUM_DIGIT_8 = "8";
+const SMB_NUM_DIGIT_9 = "9";
+const SMB_NUM_DECIMAL_POINT = ".";
+const SMB_NUM_SIGN = "s";
+const SMB_OP_ADDITION = "+";
+const SMB_OP_SUTRACTION = "-";
+const SMB_OP_MULTIPLICATION = "*";
+const SMB_OP_DIVISION = "/";
+const SMB_OP_POWER = "^";
+const SMB_CMD_CLEAR = "c";
+const SMB_CMD_CLEAR_ENTRY = "x";
+const SMB_CMD_CALCULATE = " ";
 
-  static checkSymbol(symbol) {
-    if (typeof symbol !== "string")
-      throw new TypeError("Invalid <symbol> type.");
-    if (symbol.length !== 1) throw new RangeError("Invalid <symbol> value.");
-  }
-}
+// NumberController ///////////////////////////////////////////////////////////
+const MAX_NUM_INTEGER_PART_SIZE = 6;
+const MAX_NUM_DECIMAL_PART_SIZE = 3;
 
-// Display ////////////////////////////////////////////////////////////////////
-class Display {
-  #id;
-  #domElement;
-  #symbolBuffer;
+class NumberController {
+  #isMinusSignSet;
+  #isDecimalPointSet;
+  #integerPart;
+  #decimalPart;
 
-  constructor(id) {
-    Check.checkId(id);
-    this.#id = id;
-    this.#domElement = document.getElementById(id);
-    this.#symbolBuffer = [];
-  }
-
-  get id() {
-    return this.#id;
-  }
-
-  outputSymbol(symbol) {
-    Check.checkSymbol(symbol);
-    this.#symbolBuffer.push(symbol);
-    this.#displaySymbols();
-  }
-
-  removeLastSymbol() {
-    this.#symbolBuffer.pop();
-    this.#displaySymbols();
+  constructor() {
+    this.#isMinusSignSet = false;
+    this.#isDecimalPointSet = false;
+    this.#integerPart = [];
+    this.#decimalPart = [];
   }
 
   clear() {
-    while (this.#symbolBuffer.length > 0) {
-      this.#symbolBuffer.pop();
-    }
-    this.#displaySymbols();
+    this.#isMinusSignSet = false;
+    this.#isDecimalPointSet = false;
+    this.#integerPart.length = 0;
+    this.#decimalPart.length = 0;
   }
 
-  #displaySymbols() {
-    this.#domElement.textContent = this.#symbolBuffer.join("");
+  isEmpty() {
+    return (
+      !this.#isMinusSignSet &&
+      !this.#isDecimalPointSet &&
+      this.#integerPart.length === 0 &&
+      this.#decimalPart.length === 0
+    );
+  }
+
+  getString() {
+    const sign = this.#isMinusSignSet ? "-" : "";
+    const decimalPoint = this.#isDecimalPointSet ? "." : "";
+    const integerPart = this.#integerPart.join("");
+    const decimalPart = this.#decimalPart.join("");
+    return `${sign}${integerPart}${decimalPoint}${decimalPart}`;
+  }
+
+  putSymbol(symbol) {
+    switch (symbol) {
+      case SMB_NUM_DIGIT_0:
+      case SMB_NUM_DIGIT_1:
+      case SMB_NUM_DIGIT_2:
+      case SMB_NUM_DIGIT_3:
+      case SMB_NUM_DIGIT_4:
+      case SMB_NUM_DIGIT_5:
+      case SMB_NUM_DIGIT_6:
+      case SMB_NUM_DIGIT_7:
+      case SMB_NUM_DIGIT_8:
+      case SMB_NUM_DIGIT_9: {
+        if (!this.#isDecimalPointSet) {
+          if (this.#integerPart.length < MAX_NUM_INTEGER_PART_SIZE) {
+            this.#integerPart.push(symbol);
+          }
+        } else {
+          if (this.#decimalPart.length < MAX_NUM_DECIMAL_PART_SIZE) {
+            this.#decimalPart.push(symbol);
+          }
+        }
+        break;
+      }
+      case SMB_NUM_DECIMAL_POINT: {
+        if (!this.#isDecimalPointSet) {
+          this.#isDecimalPointSet = true;
+        }
+        break;
+      }
+      case SMB_NUM_SIGN: {
+        this.#isMinusSignSet = !this.#isMinusSignSet;
+        break;
+      }
+      case SMB_CMD_CLEAR_ENTRY: {
+        if (this.#decimalPart.length > 0) {
+          this.#decimalPart.pop();
+        } else if (this.#isDecimalPointSet) {
+          this.#isDecimalPointSet = false;
+        } else if (this.#integerPart.length > 0) {
+          this.#integerPart.pop();
+        } else if (this.#isMinusSignSet) {
+          this.#isMinusSignSet = false;
+        }
+        break;
+      }
+    }
   }
 }
 
-const display = new Display("display");
+// MainController /////////////////////////////////////////////////////////////
+class MainController {
+  #operator;
+  #leftOperand;
+  #rightOperand;
 
-// InputButton ////////////////////////////////////////////////////////////////
-class InputButton {
-  #id;
+  constructor() {
+    this.#operator = undefined;
+    this.#leftOperand = new NumberController();
+    this.#rightOperand = new NumberController();
+  }
 
-  constructor(id, symbol) {
-    Check.checkId(id);
-    Check.checkSymbol(symbol);
+  clear() {
+    this.#operator = undefined;
+    this.#leftOperand.clear();
+    this.#rightOperand.clear();
+  }
 
-    this.#id = id;
+  putSymbol(symbol) {
+    switch (symbol) {
+      case SMB_OP_ADDITION:
+      case SMB_OP_SUTRACTION:
+      case SMB_OP_MULTIPLICATION:
+      case SMB_OP_SUTRACTION:
+      case SMB_OP_POWER: {
+        this.#operator = symbol;
+        break;
+      }
+      case SMB_CMD_CLEAR: {
+        this.clear();
+        break;
+      }
+      case SMB_CMD_CLEAR_ENTRY: {
+        this.#rightOperand.putSymbol(symbol);
+        break;
+      }
+      case SMB_CMD_CALCULATE: {
+        break;
+      }
+      default: {
+        this.#rightOperand.putSymbol(symbol);
+        break;
+      }
+    }
+    document.getElementById("display").textContent =
+      this.#rightOperand.getString();
+  }
+}
+
+// Main ///////////////////////////////////////////////////////////////////////
+const dataArr = [
+  ["num-digit-0", SMB_NUM_DIGIT_0],
+  ["num-digit-1", SMB_NUM_DIGIT_1],
+  ["num-digit-2", SMB_NUM_DIGIT_2],
+  ["num-digit-3", SMB_NUM_DIGIT_3],
+  ["num-digit-4", SMB_NUM_DIGIT_4],
+  ["num-digit-5", SMB_NUM_DIGIT_5],
+  ["num-digit-6", SMB_NUM_DIGIT_6],
+  ["num-digit-7", SMB_NUM_DIGIT_7],
+  ["num-digit-8", SMB_NUM_DIGIT_8],
+  ["num-digit-9", SMB_NUM_DIGIT_9],
+  ["num-decimal-point", SMB_NUM_DECIMAL_POINT],
+  ["num-sign", SMB_NUM_SIGN],
+  ["op-addition", SMB_OP_ADDITION],
+  ["op-subtraction", SMB_OP_SUTRACTION],
+  ["op-multiplication", SMB_OP_MULTIPLICATION],
+  ["op-division", SMB_OP_DIVISION],
+  ["op-power", SMB_OP_POWER],
+  ["cmd-clear", SMB_CMD_CLEAR],
+  ["cmd-clear-entry", SMB_CMD_CLEAR_ENTRY],
+  ["cmd-calculate", SMB_CMD_CALCULATE],
+];
+
+const main = () => {
+  const controller = new MainController();
+  for (const data of dataArr) {
+    const [id, symbol] = data;
+    if (!document.getElementById(id)) {
+      console.log("False id");
+    }
     document.getElementById(id).addEventListener("click", () => {
-      display.outputSymbol(symbol);
+      controller.putSymbol(symbol);
     });
   }
+};
 
-  get id() {
-    return this.#id;
-  }
-}
-
-const inputButtons = [
-  new InputButton("digit-0", "0"),
-  new InputButton("digit-1", "1"),
-  new InputButton("digit-2", "2"),
-  new InputButton("digit-3", "3"),
-  new InputButton("digit-4", "4"),
-  new InputButton("digit-5", "5"),
-  new InputButton("digit-6", "6"),
-  new InputButton("digit-7", "7"),
-  new InputButton("digit-8", "8"),
-  new InputButton("digit-9", "9"),
-  new InputButton("decimal-point", "."),
-  new InputButton("sign", "±"),
-  new InputButton("op-division", "/"),
-  new InputButton("op-multiplication", "*"),
-  new InputButton("op-subtraction", "-"),
-  new InputButton("op-addition", "+"),
-  new InputButton("op-power", "^"),
-];
-
-// CommandButton //////////////////////////////////////////////////////////////
-class CommandButton {
-  #id;
-
-  constructor(id, callback) {
-    Check.checkId(id);
-    this.#id = id;
-    document.getElementById(id).addEventListener("click", callback);
-  }
-
-  get id() {
-    return this.#id;
-  }
-}
-
-const commandButtons = [
-  new CommandButton("cmd-clear-entry", () => {
-    display.removeLastSymbol();
-  }),
-  new CommandButton("cmd-clear", () => {
-    display.clear();
-  }),
-  new CommandButton("cmd-calculate", () => {
-    alert("Calculation in progress...");
-  }),
-];
+main();
